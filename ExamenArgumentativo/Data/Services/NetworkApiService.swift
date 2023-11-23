42fe8f5cacd5f8bb62f93183e23d1fd2a8273799
JosePeblo <a01275676@tec.mx>
2023-11-23 11:42:23 -0600

    initial commit

---
+++ ExamenArgumentativo/Data/Services/NetworkApiService.swift
@@ -0,0 +1,10 @@
+//
+//  NetworkApiService.swift
+//  ExamenArgumentativo
+//
+//  Created by peblo on 23/11/23.
+//
+
+import Foundation
+import Alamofire
+
---
+++ ExamenArgumentativo/ExamenArgumentativoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenArgumentativoApp.swift
+//  ExamenArgumentativo
+//
+//  Created by peblo on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ExamenArgumentativoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
